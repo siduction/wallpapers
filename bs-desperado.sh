0f778c4449d4342e0dd7e33a9bed8c9b25528a63
Alf Gaida <info@g-com.eu>
2012-05-19 14:14:22 +0200

    * new backgrounds 4:3, thanks hendrikL * rebuild closes #708 * bs-desperado.sh: build only targeted release * refs #708

--- bs-desperado.sh
+++ bs-desperado.sh
@@ -1,58 +1,52 @@
 #!/bin/sh
-
 set -e
 
-
-RELEASES="
-    desperado:Desperado:12.1:
+RELEASE="desperado:Desperado:12.1:
 "
 # clean up obsolete stuff
-rm -f	./debian/*.install \
-	./debian/*.links \
-	./debian/*.postinst \
-	./debian/*.postrm
+rm -f ./debian/*.install \
+    ./debian/*.links \
+    ./debian/*.postinst \
+    ./debian/*.postrm
 
 #write toplevel Makefile
-for i in $RELEASES; do
-	ALL_CODENAME_SAFE="${ALL_CODENAME_SAFE} $(echo ${i} | cut -d\: -f1)"
-done
-sed	-e "s/\@ALL_CODENAME_SAFE\@/${ALL_CODENAME_SAFE}/g" \
-		./debian/templates/Makefile.in \
-			> ./Makefile
+
+ALL_CODENAME_SAFE="${ALL_CODENAME_SAFE} $(echo ${RELEASE} | cut -d\: -f1)"
+
+sed -e "s/\@ALL_CODENAME_SAFE\@/${ALL_CODENAME_SAFE}/g" \
+    ./debian/templates/Makefile.in \
+    > ./Makefile
 
 [ -d ./debian ] || exit 1
 
 TEMPLATE_CHANGELOG="./debian/templates/changelog.in"
-sed     -e s/\@CODENAME_SAFE\@/$(echo ${i} | cut -d\: -f1)/g \
-              ${TEMPLATE_CHANGELOG} > ./debian/changelog
+sed -e s/\@CODENAME_SAFE\@/$(echo ${RELEASE} | cut -d\: -f1)/g \
+    ${TEMPLATE_CHANGELOG} > ./debian/changelog
 
 TEMPLATE_SRC="./debian/templates/control.source.in"
-sed     -e s/\@CODENAME_SAFE\@/$(echo ${i} | cut -d\: -f1)/g \
+sed     -e s/\@CODENAME_SAFE\@/$(echo ${RELEASE} | cut -d\: -f1)/g \
               ${TEMPLATE_SRC} > ./debian/control
 
-for i in $RELEASES; do
+# write debian/control from templates
+TEMPLATES_BIN="./debian/templates/control.binary.in"
 
-	# write debian/control from templates
-	TEMPLATES_BIN="./debian/templates/control.binary.in"
+sed -e s/\@CODENAME_SAFE\@/$(echo ${RELEASE} | cut -d\: -f1)/g \
+    -e s/\@CODENAME\@/$(echo ${RELEASE} | cut -d\: -f2)/g \
+    -e s/\@VERSION\@/$(echo ${RELEASE} | cut -d\: -f3)/g \
+    ${TEMPLATES_BIN} >> ./debian/control
 
-	sed	-e s/\@CODENAME_SAFE\@/$(echo ${i} | cut -d\: -f1)/g \
-		-e s/\@CODENAME\@/$(echo ${i} | cut -d\: -f2)/g \
-		-e s/\@VERSION\@/$(echo ${i} | cut -d\: -f3)/g \
-			${TEMPLATES_BIN} >> ./debian/control
-
-	# write debian/*.install from templates
-	for j in kde kdm ksplash wallpaper xfce xsplash; do
-		if [ -r  ./debian/templates/siduction-art-${j}-CODENAME_SAFE.install.in ]; then
-			sed	-e s/\@CODENAME_SAFE\@/$(echo ${i} | cut -d\: -f1)/g \
-					./debian/templates/siduction-art-${j}-CODENAME_SAFE.install.in \
-						> ./debian/siduction-art-${j}-$(echo ${i} | cut -d\: -f1).install
-		else
-			continue
-		fi
-	done
-
-	# link KDE4 style wallpapers to /usr/share/wallpapers/
-	sed	-e s/\@CODENAME_SAFE\@/$(echo ${i} | cut -d\: -f1)/g \
-			./debian/templates/siduction-art-wallpaper-CODENAME_SAFE.links.in \
-				> ./debian/siduction-art-wallpaper-$(echo ${i} | cut -d\: -f1).links
+# write debian/*.install from templates
+for k in kde kdm ksplash wallpaper xfce xsplash; do
+    if [ -r  ./debian/templates/siduction-art-${k}-CODENAME_SAFE.install.in ]; then
+        sed -e s/\@CODENAME_SAFE\@/$(echo ${RELEASE} | cut -d\: -f1)/g \
+            ./debian/templates/siduction-art-${k}-CODENAME_SAFE.install.in \
+            > ./debian/siduction-art-${k}-$(echo ${RELEASE} | cut -d\: -f1).install
+    else
+        continue
+    fi
 done
+
+# link KDE4 style wallpapers to /usr/share/wallpapers/
+sed -e s/\@CODENAME_SAFE\@/$(echo ${RELEASE} | cut -d\: -f1)/g \
+    ./debian/templates/siduction-art-wallpaper-CODENAME_SAFE.links.in \
+    > ./debian/siduction-art-wallpaper-$(echo ${RELEASE} | cut -d\: -f1).links
