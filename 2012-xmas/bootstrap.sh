2f9b2401dfce9d7c681d840e33602861f3d2c5b9
Alf Gaida <info@g-com.eu>
2012-12-22 14:12:55 +0100

    refs #1050 new scripting

--- 2012-xmas/bootstrap.sh
+++ 2012-xmas/bootstrap.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -e
 
-RELEASE="xmas:Xmas-2012:12-2.0"
+if [ -f VERSION ]; then
+    . ./VERSION
+else
+    echo "No VERSION-File, exit!"
+    exit 1
+fi
 
 # clean up obsolete stuff
 rm -f ./debian/*.install \
@@ -11,7 +16,7 @@ rm -f ./debian/*.install \
 
 #write toplevel Makefile
 
-ALL_CODENAME_SAFE="${ALL_CODENAME_SAFE} $(echo ${RELEASE} | cut -d\: -f1)"
+ALL_CODENAME_SAFE="${ALL_CODENAME_SAFE} ${NAME}"
 
 sed -e "s/\@ALL_CODENAME_SAFE\@/${ALL_CODENAME_SAFE}/g" \
     ./debian/templates/Makefile.in \
@@ -21,28 +26,28 @@ sed -e "s/\@ALL_CODENAME_SAFE\@/${ALL_CODENAME_SAFE}/g" \
 
 TEMPLATE_CHANGELOG="./debian/templates/changelog.in"
 if [ ! -e ./debian/changelog ]; then 
-sed -e s/\@CODENAME_SAFE\@/$(echo ${RELEASE} | cut -d\: -f1)/g \
+    sed -e s/\@CODENAME_SAFE\@/${NAME}/g \
     ${TEMPLATE_CHANGELOG} > ./debian/changelog
 fi
 
 TEMPLATE_SRC="./debian/templates/control.source.in"
-sed     -e s/\@CODENAME_SAFE\@/$(echo ${RELEASE} | cut -d\: -f1)/g \
+sed     -e s/\@CODENAME_SAFE\@/${NAME}/g \
               ${TEMPLATE_SRC} > ./debian/control
 
 # write debian/control from templates
 TEMPLATES_BIN="./debian/templates/control.binary.in"
 
-sed -e s/\@CODENAME_SAFE\@/$(echo ${RELEASE} | cut -d\: -f1)/g \
-    -e s/\@CODENAME\@/$(echo ${RELEASE} | cut -d\: -f2)/g \
-    -e s/\@VERSION\@/$(echo ${RELEASE} | cut -d\: -f3)/g \
+sed -e s/\@CODENAME_SAFE\@/${NAME}/g \
+    -e s/\@CODENAME\@/${DESCRIPTION}/g \
+    -e s/\@VERSION\@/${VERSION}/g \
     ${TEMPLATES_BIN} >> ./debian/control
 
 # write debian/*.install from templates
 for k in kde kdm ksplash lightdm rqt wallpaper xfce xsplash; do
     if [ -r  ./debian/templates/siduction-art-${k}-CODENAME_SAFE.install.in ]; then
-        sed -e s/\@CODENAME_SAFE\@/$(echo ${RELEASE} | cut -d\: -f1)/g \
+        sed -e s/\@CODENAME_SAFE\@/${NAME}/g \
             ./debian/templates/siduction-art-${k}-CODENAME_SAFE.install.in \
-            > ./debian/siduction-art-${k}-$(echo ${RELEASE} | cut -d\: -f1).install
+            > ./debian/siduction-art-${k}-${NAME}.install
     else
         continue
     fi
@@ -51,9 +56,9 @@ done
 # write debian/*.postinst from templates
 for k in kde kdm ksplash lightdm rqt wallpaper xfce xsplash; do
     if [ -r  ./debian/templates/siduction-art-${k}-CODENAME_SAFE.postinst.in ]; then
-        sed -e s/\@CODENAME_SAFE\@/$(echo ${RELEASE} | cut -d\: -f1)/g \
+        sed -e s/\@CODENAME_SAFE\@/${NAME}/g \
             ./debian/templates/siduction-art-${k}-CODENAME_SAFE.postinst.in \
-            > ./debian/siduction-art-${k}-$(echo ${RELEASE} | cut -d\: -f1).postinst
+            > ./debian/siduction-art-${k}-${NAME}.postinst
     else
         continue
     fi
@@ -62,9 +67,9 @@ done
 # write debian/*.postrm from templates
 for k in kde kdm ksplash lightdm rqt wallpaper xfce xsplash; do
     if [ -r  ./debian/templates/siduction-art-${k}-CODENAME_SAFE.postrm.in ]; then
-        sed -e s/\@CODENAME_SAFE\@/$(echo ${RELEASE} | cut -d\: -f1)/g \
+        sed -e s/\@CODENAME_SAFE\@/${NAME}/g \
             ./debian/templates/siduction-art-${k}-CODENAME_SAFE.postrm.in \
-            > ./debian/siduction-art-${k}-$(echo ${RELEASE} | cut -d\: -f1).postrm
+            > ./debian/siduction-art-${k}-${NAME}.postrm
     else
         continue
     fi
@@ -73,15 +78,15 @@ done
 # write debian/*.postrm from templates
 for k in kde kdm ksplash lightdm rqt wallpaper xfce xsplash; do
     if [ -r  ./debian/templates/siduction-art-${k}-CODENAME_SAFE.links.in ]; then
-        sed -e s/\@CODENAME_SAFE\@/$(echo ${RELEASE} | cut -d\: -f1)/g \
+        sed -e s/\@CODENAME_SAFE\@/${NAME}/g \
             ./debian/templates/siduction-art-${k}-CODENAME_SAFE.links.in \
-            > ./debian/siduction-art-${k}-$(echo ${RELEASE} | cut -d\: -f1).links
+            > ./debian/siduction-art-${k}-${NAME}.links
     else
         continue
     fi
 done
 
 ## grub theme
-sed -e s/\@CODENAME_SAFE\@/$(echo ${RELEASE} | cut -d\: -f1)/g \
+sed -e s/\@CODENAME_SAFE\@/${NAME}/g \
     ./debian/templates/grub-theme-siduction-CODENAME_SAFE.install.in \
-    > ./debian/grub-theme-siduction-$(echo ${RELEASE} | cut -d\: -f1).install
+    > ./debian/grub-theme-siduction-${NAME}.install
