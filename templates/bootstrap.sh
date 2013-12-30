b198377df3ca543bece42f3fbaba964c9c769375
Alf Gaida <agaida@siduction.org>
2013-12-30 21:09:48 +0100

    yeah - final refs #1448

--- templates/bootstrap.sh
+++ templates/bootstrap.sh
@@ -136,14 +136,6 @@ sed -e "s/\@CODENAME_SAFE\@/${NAME}/g" \
     > ./artwork/splash-xfce/themerc
 
 sed -e "s/\@CODENAME_SAFE\@/${NAME}/g" \
-    ../templates/artwork/theme-kde/CODENAME_SAFE.colors.in \
-    > ./artwork/theme-kde/${NAME}.colors
-
-sed -e "s/\@CODENAME_SAFE\@/${NAME}/g" \
-    ../templates/artwork/theme-kde/CODENAME_SAFE-dark.colors.in \
-    > ./artwork/theme-kde/${NAME}-dark.colors
-
-sed -e "s/\@CODENAME_SAFE\@/${NAME}/g" \
     ../templates/artwork/wallpaper/metadata.desktop.in \
     > ./artwork/wallpaper/metadata.desktop
 
