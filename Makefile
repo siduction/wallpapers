8e12d51ad285829088d5eb8c59a1a62a88caa05a
Torsten Wohlfarth <towo@siduction.org>
2024-12-16 10:52:14 +0100

    light and dark, fix packaging

--- Makefile
+++ Makefile
@@ -8,8 +8,13 @@ all:
 	mkdir -p build/${CODENAME_SAFE}
 	cp metadata.* build/${CODENAME_SAFE}
 	mkdir -p build/${CODENAME_SAFE}/contents/images
-	cp png/*.png build/${CODENAME_SAFE}/contents/images
+	mkdir -p build/${CODENAME_SAFE}/contents/images_dark
+	cp png/*.png build/${CODENAME_SAFE}/contents/images/
+	cp png-dark/*.png build/${CODENAME_SAFE}/contents/images_dark/
 	rm -f png/*.png
+	rm -f png-dark/*.png
 
 clean:
 	$(RM) -r build/
+	$(RM) -r png/
+	$(RM) -r png-dark/
--- make-png-from-svg.sh
+++ make-png-from-svg.sh
@@ -7,16 +7,44 @@ FORMATE_16x9=(1280x720 1366x768 1920x1080 2560x1440)
 FORMATE_16x10=(1280x800 1440x900 1680x1050 1920x1200)
 FORMATE_4x3=(1280x960 1400x1050 1600x1200 2048x1536)
 
+mkdir png
+mkdir png-dark
+
+
 cd ./svg
 
-#convert -resize 1920x1080 shineon-16x9.svg ../png/background.png
+convert -resize 1920x1080 shineon-16x9.svg ../png/background.png
 #convert -resize 400x250 shineon-16x9.svg ../png/screenshot.png
 
 convert -resize 3840x2160 shineon-16x9.svg ../png/3840x2160.png
 convert -resize 2560x1600 shineon-16x10.svg ../png/2560x1600.png
 convert -resize 2732x2048 shineon-4x3.svg ../png/2732x2048.png
 
+cd ../svg-dark
+
+convert -resize 400x250 shineon-16x9.svg ../png-dark/background.png
+
+convert -resize 3840x2160 shineon-16x9.svg ../png-dark/3840x2160.png
+convert -resize 2560x1600 shineon-16x10.svg ../png-dark/2560x1600.png
+convert -resize 2732x2048 shineon-4x3.svg ../png-dark/2732x2048.png
+
+
 cd ../png
+
+for r in ${FORMATE_16x9[*]}; do
+    convert -resize $r 3840x2160.png $r.png
+done
+unset r
+for r in ${FORMATE_16x10[*]}; do
+    convert -resize $r 2560x1600.png $r.png
+done
+unset r
+for r in ${FORMATE_4x3[*]}; do
+    convert -resize $r 2732x2048.png $r.png
+done
+unset r
+
+cd ../png-dark
 
 for r in ${FORMATE_16x9[*]}; do
     convert -resize $r 3840x2160.png $r.png
