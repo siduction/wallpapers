1478521b1071357921568b7facc04fd8a77373a5
Hendrik Lehmbruch <hendrikL@siduction.org>
2026-03-09 12:00:32 +0100

    closes #1, added 4k and 8k format

--- make-jpg-from-svg.sh
+++ make-jpg-from-svg.sh
@@ -6,6 +6,11 @@
 FORMATE_16x9=(1280x720 1366x768 1920x1080 2560x1440 3840x2160)
 FORMATE_16x10=(1280x800 1440x900 1680x1050 1920x1200 2560x1600)
 FORMATE_4x3=(1280x960 1400x1050 1600x1200 2048x1536 2732x2048)
+FORMATE_4k=(4096x2160)
+FORMATE_8k=(7680x4320)
+
+mkdir jpg
+mkdir jpg-dark
 
 cd ./svg
 for i in $(basename -s .svg *.svg); do
@@ -28,6 +33,57 @@ for i in $(basename -s .svg *.svg); do
 	"4x3")
 		for r in ${FORMATE_4x3[*]}; do
 			convert -resize $r $i.svg ../jpg/$r.jpg
+		done
+		unset r
+		;;
+	"4k")
+		for r in ${FORMATE_4k[*]}; do
+			convert -resize $r $i.svg ../jpg/$r.jpg
+		done
+		unset r
+		;;
+	"8k")
+		for r in ${FORMATE_8k[*]}; do
+			convert -resize $r $i.svg ../jpg/$r.jpg
+		done
+		unset r
+		;;
+	esac
+done
+
+cd ../svg-dark
+for i in $(basename -s .svg *.svg); do
+
+	f=$(echo $i | sed s'!.*-!!')
+
+	case $f in
+	"16x9")
+		for r in ${FORMATE_16x9[*]}; do
+			convert -resize $r $i.svg ../jpg-dark/$r.jpg
+		done
+		unset r
+		;;
+	"16x10")
+		for r in ${FORMATE_16x10[*]}; do
+			convert -resize $r $i.svg ../jpg-dark/$r.jpg
+		done
+		unset r
+		;;
+	"4x3")
+		for r in ${FORMATE_4x3[*]}; do
+			convert -resize $r $i.svg ../jpg-dark/$r.jpg
+		done
+		unset r
+		;;
+	"4k")
+		for r in ${FORMATE_4k[*]}; do
+			convert -resize $r $i.svg ../jpg-dark/$r.jpg
+		done
+		unset r
+		;;
+	"8k")
+		for r in ${FORMATE_8k[*]}; do
+			convert -resize $r $i.svg ../jpg-dark/$r.jpg
 		done
 		unset r
 		;;
--- make-png-from-svg.sh
+++ make-png-from-svg.sh
@@ -6,33 +6,39 @@
 FORMATE_16x9=(1280x720 1366x768 1920x1080 2560x1440)
 FORMATE_16x10=(1280x800 1440x900 1680x1050 1920x1200)
 FORMATE_4x3=(1280x960 1400x1050 1600x1200 2048x1536)
+FORMATE_4k=(4096x2160)
+FORMATE_8k=(7680x4320)
 
 mkdir png
 mkdir png-dark
-mkdir png-xmas
-mkdir png-dark-xmas
+# mkdir png-xmas
+# mkdir png-dark-xmas
 
 # default
-convert -resize 1920x1080 ./svg/shineon-16x9.svg ./png/background.png
-convert -resize 3840x2160 ./svg/shineon-16x9.svg ./png/3840x2160.png
-convert -resize 2560x1600 ./svg/shineon-16x10.svg ./png/2560x1600.png
-convert -resize 2732x2048 ./svg/shineon-4x3.svg ./png/2732x2048.png
+convert -resize 1920x1080 ./svg/Big-Crime-16x9.svg ./png/background.png
+convert -resize 3840x2160 ./svg/Big-Crime-16x9.svg ./png/3840x2160.png
+convert -resize 2560x1600 ./svg/Big-Crime-16x10.svg ./png/2560x1600.png
+convert -resize 2732x2048 ./svg/Big-Crime-4x3.svg ./png/2732x2048.png
+convert -resize 4096x2160 ./svg/Big-Crime-4k.svg ./png/4096x2160.png
+convert -resize 7680x4320 ./svg/Big-Crime-8k.svg ./png/7680x4320.png
 
-convert -resize 1920x1080 ./svg-dark/shineon-16x9.svg ./png-dark/background.png
-convert -resize 3840x2160 ./svg-dark/shineon-16x9.svg ./png-dark/3840x2160.png
-convert -resize 2560x1600 ./svg-dark/shineon-16x10.svg ./png-dark/2560x1600.png
-convert -resize 2732x2048 ./svg-dark/shineon-4x3.svg ./png-dark/2732x2048.png
+convert -resize 1920x1080 ./svg-dark/Big-Crime-16x9.svg ./png-dark/background.png
+convert -resize 3840x2160 ./svg-dark/Big-Crime-16x9.svg ./png-dark/3840x2160.png
+convert -resize 2560x1600 ./svg-dark/Big-Crime-16x10.svg ./png-dark/2560x1600.png
+convert -resize 2732x2048 ./svg-dark/Big-Crime-4x3.svg ./png-dark/2732x2048.png
+convert -resize 4096x2160 ./svg-dark/Big-Crime-4k.svg ./png-dark/4096x2160.png
+convert -resize 7680x4320 ./svg-dark/Big-Crime-8k.svg ./png-dark/7680x4320.png
 
 # x-mas
-convert -resize 1920x1080 ./x-mas/svg/shineon-16x9.svg ./png-xmas/background.png
-convert -resize 3840x2160 ./x-mas/svg/shineon-16x9.svg ./png-xmas/3840x2160.png
-convert -resize 2560x1600 ./x-mas/svg/shineon-16x10.svg ./png-xmas/2560x1600.png
-convert -resize 2732x2048 ./x-mas/svg/shineon-4x3.svg ./png-xmas/2732x2048.png
-
-convert -resize 1920x1080 ./x-mas/svg-dark/shineon-16x9.svg ./png-dark-xmas/background.png
-convert -resize 3840x2160 ./x-mas/svg-dark/shineon-16x9.svg ./png-dark-xmas/3840x2160.png
-convert -resize 2560x1600 ./x-mas/svg-dark/shineon-16x10.svg ./png-dark-xmas/2560x1600.png
-convert -resize 2732x2048 ./x-mas/svg-dark/shineon-4x3.svg ./png-dark-xmas/2732x2048.png
+# convert -resize 1920x1080 ./x-mas/svg/Big-Crime-16x9.svg ./png-xmas/background.png
+# convert -resize 3840x2160 ./x-mas/svg/Big-Crime-16x9.svg ./png-xmas/3840x2160.png
+# convert -resize 2560x1600 ./x-mas/svg/Big-Crime-16x10.svg ./png-xmas/2560x1600.png
+# convert -resize 2732x2048 ./x-mas/svg/Big-Crime-4x3.svg ./png-xmas/2732x2048.png
+#
+# convert -resize 1920x1080 ./x-mas/svg-dark/Big-Crime-16x9.svg ./png-dark-xmas/background.png
+# convert -resize 3840x2160 ./x-mas/svg-dark/Big-Crime-16x9.svg ./png-dark-xmas/3840x2160.png
+# convert -resize 2560x1600 ./x-mas/svg-dark/Big-Crime-16x10.svg ./png-dark-xmas/2560x1600.png
+# convert -resize 2732x2048 ./x-mas/svg-dark/Big-Crime-4x3.svg ./png-dark-xmas/2732x2048.png
 
 # default
 cd ./png
@@ -47,6 +53,14 @@ done
 unset r
 for r in ${FORMATE_4x3[*]}; do
     convert -resize $r 2732x2048.png $r.png
+done
+unset r
+for r in ${FORMATE_4k[*]}; do
+    convert -resize $r 4096x2160.png $r.png
+done
+unset r
+for r in ${FORMATE_8k[*]}; do
+    convert -resize $r 7680x4320.png $r.png
 done
 unset r
 
@@ -64,36 +78,44 @@ for r in ${FORMATE_4x3[*]}; do
     convert -resize $r 2732x2048.png $r.png
 done
 unset r
+for r in ${FORMATE_4k[*]}; do
+    convert -resize $r 4096x2160.png $r.png
+done
+unset r
+for r in ${FORMATE_8k[*]}; do
+    convert -resize $r 7680x4320.png $r.png
+done
+unset r
 
 # x-mas
-cd ../png-xmas
-
-for r in ${FORMATE_16x9[*]}; do
-    convert -resize $r 3840x2160.png $r.png
-done
-unset r
-for r in ${FORMATE_16x10[*]}; do
-    convert -resize $r 2560x1600.png $r.png
-done
-unset r
-for r in ${FORMATE_4x3[*]}; do
-    convert -resize $r 2732x2048.png $r.png
-done
-unset r
-
-cd ../png-dark-xmas
-
-for r in ${FORMATE_16x9[*]}; do
-    convert -resize $r 3840x2160.png $r.png
-done
-unset r
-for r in ${FORMATE_16x10[*]}; do
-    convert -resize $r 2560x1600.png $r.png
-done
-unset r
-for r in ${FORMATE_4x3[*]}; do
-    convert -resize $r 2732x2048.png $r.png
-done
-unset r
+# cd ../png-xmas
+#
+# for r in ${FORMATE_16x9[*]}; do
+#     convert -resize $r 3840x2160.png $r.png
+# done
+# unset r
+# for r in ${FORMATE_16x10[*]}; do
+#     convert -resize $r 2560x1600.png $r.png
+# done
+# unset r
+# for r in ${FORMATE_4x3[*]}; do
+#     convert -resize $r 2732x2048.png $r.png
+# done
+# unset r
+#
+# cd ../png-dark-xmas
+#
+# for r in ${FORMATE_16x9[*]}; do
+#     convert -resize $r 3840x2160.png $r.png
+# done
+# unset r
+# for r in ${FORMATE_16x10[*]}; do
+#     convert -resize $r 2560x1600.png $r.png
+# done
+# unset r
+# for r in ${FORMATE_4x3[*]}; do
+#     convert -resize $r 2732x2048.png $r.png
+# done
+# unset r
 
 exit 0
